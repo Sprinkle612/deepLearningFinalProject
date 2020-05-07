c2cee6986709adbdd2dabc22b22c8f1cf3e92e06
rainyleon <44528182+Sprinkle612@users.noreply.github.com>
2020-05-07 17:16:26 -0400

    edge loss

---
+++ EdgeLoss.py
@@ -0,0 +1,37 @@
+from __future__ import division
+from torchvision import models
+from torchvision import transforms
+from PIL import Image
+import argparse
+import torch
+import torchvision
+import torch.nn as nn
+import numpy as np
+import torch.utils.model_zoo as model_zoo
+
+class Img_embed(nn.Module):
+    def __init__(self, input_size, hidden_dim, output_size):
+        super(Img_embed, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_dim)
+        self.relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(hidden_dim, output_size)
+
+    def forward(self,x):
+        x = x.view(1, -1)
+        output = self.fc1(x)
+        output = self.relu(output)
+        output = F.normalize(self.fc2(output), dim=0, p=2)
+
+        return output
+
+class IMGNet(nn.Module):
+    def __init__(self):
+        self.model = Img_embed(256**2, 64**2, 300)
+        self.model.load_state_dict("../models/img_model_best.pth")
+
+        for param in self.model.parameters():
+        	param.resquires_grad = False
+        print('Load pretrained img_embed model succeed')
+    
+    def forward(self, x):
+        return self.model(x)
